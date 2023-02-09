faa9af61a8381e00089fbe2cb657d62f416bf001
coolcoder149 <cnchandlersford@gmail.com>
2023-02-09 17:34:55 +0000

    amazing ninja worlds

---
+++ Amazing Ninja Worlds/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothingTime = .2f;
+    public Vector3 cameraOffset;
+    private Vector3 _velocity;
+
+    void Update()
+    {
+        Vector3 targetPos = new Vector3(target.position.x, target.position.y,
+            transform.position.z) + cameraOffset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos,
+          ref _velocity, smoothingTime);
+    }
+
+    void Start()
+    {
+    }
+}
---
+++ Amazing Ninja Worlds/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+
+    public float groundDistanceThreshold = 0.55f;
+
+    public LayerMask whatIsGround;
+
+    private bool _isGrounded;
+    private Rigidbody2D _rigidbody;
+
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        _isGrounded = Physics2D.Raycast(transform.position, Vector2.down,
+            groundDistanceThreshold, whatIsGround);
+
+        if (_isGrounded && Input.GetButtonDown("Jump"))
+        {
+            _rigidbody.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float movement = moveSpeed * Input.GetAxisRaw("Horizontal");
+
+        _rigidbody.position += movement * Time.deltaTime * Vector2.right;
+    }
+}
