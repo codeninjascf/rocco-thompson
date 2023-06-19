1c0f621b9e2cb7c6ccd0198b843223242807dd06
codeninjascf <cnchandlersford@gmail.com>
2023-06-19 15:25:46 +0100

    dasad
    Page 6 MV : Unit 2 Images Page 2: The Player

--- Meteoridvania/Assets/Scripts/PlayerController.cs
+++ Meteoridvania/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,11 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D theRB;
     public float moveSpeed;
     public float jumpForce;
+    public Transoform groundPoint;
+    private bool isOnGround;
+    public LayerMask whatIsGround;
+
+
 
     
         // Start is called before the first frame update
@@ -17,8 +22,14 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {theRB.velocity = new Vector2(Input.GetAxisRaw("H"))
         theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, theRB.velocity.y);
-
+        if (Input.GetButtonDown("Jump"))
+        {
+            theRB.velocity = new Vector2(theRB.velocity.x,jumpForce);
+        }
+       
     }
 }
+
+    
