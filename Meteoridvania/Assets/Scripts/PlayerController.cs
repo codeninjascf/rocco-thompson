66d4e794df52291d5b4c64c3629fdc63eb6f45d8
codeninjascf <cnchandlersford@gmail.com>
2023-08-01 12:39:13 +0100

    2901222222222222222222222222222222222222222222222
    good changes

--- Meteoridvania/Assets/Scripts/PlayerController.cs
+++ Meteoridvania/Assets/Scripts/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour
     private bool isOnGround;
     public LayerMask whatIsGround;
 
+    public Animator anim;
+
 
 
     
@@ -28,7 +30,8 @@ public class PlayerController : MonoBehaviour
         {
             theRB.velocity = new Vector2(theRB.velocity.x,jumpForce);
         }
-       
+        anim.SetBool("isOnGround", isOnGround);
+        anim.SetFloat("speed", Mathf.Abs(theRB.velocity.x));
     }
 }
 
