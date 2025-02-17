3fb6cf7458af31a28fe9b7fd5401e592a2f0da31
codeninjascf <105656685+codeninjascf@users.noreply.github.com>
2025-02-17 11:54:04 +0000

    Deleted Meteroidvania  (Redo)

--- Meteroidvania (Redo)/Assets/Scripts/PlayerAbilityTracker.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerAbilityTracker : MonoBehaviour
-{
-    public bool canDoubleJump, canDash, canBecomeBall, canDropBomb;
-}
